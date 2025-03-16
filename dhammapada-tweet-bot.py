b6e70cf547808bbfec20c5dac3433413aaafa3a6
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-03-16 15:07:54 -0300

    commit

--- dhammapada-tweet-bot.py
+++ dhammapada-tweet-bot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+#!/home/u07/.venv/bin/python
+
 import os
 import json
 import random
@@ -8,25 +10,43 @@ import tweepy
 
 import dhammapada_tweet_bot_credentials as creds
 
-LAST_ID_FILEPATH = os.path.expanduser("~/.dhammapada-tweet-bot.lastid")
+PREVIOUS_IDS_FILEPATH = os.path.expanduser("~/.dhammapada-tweet-bot.previous_ids")
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 DHAMMAPADA_JSON_FILEPATH = f"{SCRIPT_PATH}/dhammapada.json"
 
+def chunk_string_by_words(text, max_chars):
+    words = text.split(' ')
+    chunks = []
+    current_chunk = ""
+    for word in words:
+        if not current_chunk:
+            current_chunk = word
+        elif len(current_chunk) + len(word) + 1 <= max_chars:
+            current_chunk += " " + word
+        else:
+            chunks.append(current_chunk)
+            current_chunk = word
+    if current_chunk:
+        chunks.append(current_chunk)
+    return chunks
 
-def get_last_id(last_id_filepath=LAST_ID_FILEPATH):
-    if not os.path.isfile(last_id_filepath):
+
+def get_previous_ids(previous_ids_filepath=PREVIOUS_IDS_FILEPATH):
+    if not os.path.isfile(previous_ids_filepath):
         return None
-    with open(last_id_filepath, "r") as lidfp:
-        last_id = lidfp.read()
-    return last_id
+    with open(previous_ids_filepath, "r") as lidfp:
+        previous_ids = lidfp.readlines()
+    return previous_ids
 
 
-def set_last_id(id, last_id_filepath=LAST_ID_FILEPATH):
-    with open(last_id_filepath, "w") as lidfp:
-        lidfp.write(id)
-    with open(last_id_filepath, "r") as lidfp:
-        last_id = lidfp.read()
-    return last_id
+def set_previous_id(id_list, previous_ids_filepath=PREVIOUS_IDS_FILEPATH):
+    ids = "\n".join([str(item) for item in id_list])
+
+    with open(previous_ids_filepath, "w") as lidfp:
+        lidfp.write(ids)
+    with open(previous_ids_filepath, "r") as lidfp:
+        previous_ids = lidfp.read()
+    return previous_ids
 
 
 def get_verse():
@@ -34,7 +54,8 @@ def get_verse():
         dhammapada_json = json.load(dhammapada_json_file)
 
     keys = dhammapada_json.keys()
-    random_choice = random.choice(list(keys))
+    #  random_choice = random.choice(list(keys))
+    random_choice = '100'
 
     return dhammapada_json[random_choice]
 
@@ -49,22 +70,38 @@ message = f"""\
 {signature} \
 """
 
+chunks = chunk_string_by_words(text=message, max_chars=260)
+
 print(message)
+
 
 client = tweepy.Client(consumer_key=creds.CONSUMER_KEY,
                        consumer_secret=creds.CONSUMER_SECRET,
                        access_token=creds.ACCESS_TOKEN,
                        access_token_secret=creds.ACCESS_TOKEN_SECRET)
 
-response = client.create_tweet(text=message)
+id_list = list()
+
+for idx, chunk in enumerate(chunks):
+    if idx == 0:
+        #  response = client.create_tweet(text=message)
+        response = client.create_tweet(text=chunk)
+        new_post_id = response.data['id']  # pyright: ignore
+        id_list.append(response.data['id']) # pyright: ignore
+    else:
+        response = client.create_tweet(text=chunk,
+                                       in_reply_to_tweet_id=new_post_id)
+        new_post_id = response.data['id']  # pyright: ignore
+        id_list.append(response.data['id']) # pyright: ignore
 
 # id of the last tweet, to be deleted
-last_id = get_last_id(last_id_filepath=LAST_ID_FILEPATH)
-if last_id:
-    delete_response = client.delete_tweet(id=last_id)
-    print(delete_response.data)  # pyright: ignore
+previous_ids = get_previous_ids(previous_ids_filepath=PREVIOUS_IDS_FILEPATH)
+if previous_ids:
+    for item in previous_ids:
+        delete_response = client.delete_tweet(id=item)
+        print(delete_response.data)  # pyright: ignore
 
 new_post_id = response.data['id']  # pyright: ignore
-set_last_id(new_post_id, last_id_filepath=LAST_ID_FILEPATH)
+set_previous_id(id_list, previous_ids_filepath=PREVIOUS_IDS_FILEPATH)
 
 print(response.data)  # pyright: ignore
